9489d8bc9894c88a2047a18cc167657ebe4701d3
Purveshpund <purvesh.pund.work@gmail.com>
2021-10-20 13:10:55 +0530

    third commit

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -17,9 +17,9 @@ public class LoginPageTest extends TestBase{
 
 	Logger logger;
 	
-	public LoginPageTest(){
-		super();
-	}
+	//public LoginPageTest(){
+	//	super();
+	//}
 	
 	@BeforeMethod
 	public void setUp(){
